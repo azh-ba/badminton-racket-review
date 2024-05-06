40db6aefc6c29de5372ade9e2f7d423d7b40f144
azh-ba <anhbanguyenquoc@gmail.com>
2024-05-06 10:07:15 +0700

    Update

--- src/app/components/rackets/rackets-search/rackets-search.component.ts
+++ src/app/components/rackets/rackets-search/rackets-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Racket } from 'src/app/racket';
 import { RacketService } from 'src/app/services/racket.service';
@@ -14,7 +15,7 @@ export class RacketsSearchComponent implements OnInit {
   rackets$!: Observable<Racket[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private racketService: RacketService) { }
+  constructor(private racketService: RacketService, private router: Router) { }
 
   // push a search term into the observable stream
   search(term: string): void {
@@ -22,15 +23,15 @@ export class RacketsSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.rackets$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+    // this.rackets$ = this.searchTerms.pipe(
+    //   // wait 300ms after each keystroke before considering the term
+    //   debounceTime(300),
 
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
+    //   // ignore new term if same as previous term
+    //   distinctUntilChanged(),
 
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.racketService.searchRackets(term)),
-    );
+    //   // switch to new search observable each time the term changes
+    //   switchMap((term: string) => this.racketService.searchRackets(term)),
+    // );    
   }
 }
